506982b92992faa93704c7f9ddb79e4ca974d084
Aurelius Wendelken <webtobesocial@gmail.com>
2025-04-03 08:20:16 +0200

    add table definition for school type and inserts

--- data/sh_school_schema.sql
+++ data/sh_school_schema.sql
@@ -15,11 +15,45 @@ CREATE TABLE IF NOT EXISTS sh_school (
   longitude NUMERIC,
   latitude NUMERIC,
   agency_number VARCHAR,
-  main_school_type INT,
-  school_type INT,
+  main_school_type INTEGER CHECK (main_school_type IN (
+      1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192
+  )),
+  school_type INTEGER CHECK (school_type >= 0),
   slug VARCHAR,
-  wkb_geometry GEOMETRY(POINT, 4326)
+  wkb_geometry GEOMETRY(Point, 4326)
 );
+
+
+-- TABELLE SCHULTYPEN SCHLESWIG-HOLSTEIN
+DROP TABLE IF EXISTS sh_school_type CASCADE;
+
+CREATE TABLE IF NOT EXISTS sh_school_type (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR NOT NULL,
+    code INTEGER NOT NULL UNIQUE CHECK (
+        code IN (1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192)
+    )
+);
+
+INSERT INTO sh_school_type (name, code) VALUES
+  ('Grundschule (GS)', 1),
+  ('Hauptschule (HS)', 2),
+  ('Grund- und Hauptschule (GHS)', 4),
+  ('Förderzentrum (FöZ)', 8),
+  ('Realschule (RS)', 16),
+  ('Gymnasium (Gym)', 32),
+  ('Gesamtschule (GesS)', 64),
+  ('Berufsbildende Schule (BBS)', 128),
+  ('Schulen in freier Trägerschaft', 256),
+  ('Regionalschule (RegS)', 512),
+  ('Gemeinschaftsschule (GemS)', 1024),
+  ('Gemeinschaftsschule mit Oberstufe', 2048),
+  ('Ganztagsschule - offen', 4096),
+  ('Ganztagsschule - gebunden', 8192);
+
+
+-- INDEX
+CREATE INDEX IF NOT EXISTS idx_sh_school_type_code ON sh_school_type (code);
 
 -- GEOMETRY INDEX
 CREATE INDEX IF NOT EXISTS idx_sh_school_geometry ON sh_school USING GIST (wkb_geometry);
